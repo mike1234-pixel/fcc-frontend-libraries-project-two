50fa357d8ea5f308013e925f8c7a5a3e1890e080
Michael Tandy <mike.tandy.1@gmail.com>
2020-11-13 17:10:14 +0000

    Updates

---
+++ precache-manifest.c522200b904a88f696ccc4970bc38004.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78dc4b8824eefe605e8b0ddb242c246c",
+    "url": "/fcc-frontend-libraries-project-two/index.html"
+  },
+  {
+    "revision": "b308f479e6deb26bbe9d",
+    "url": "/fcc-frontend-libraries-project-two/static/css/main.3497258f.chunk.css"
+  },
+  {
+    "revision": "63586706ed22d8f164b3",
+    "url": "/fcc-frontend-libraries-project-two/static/js/2.7e46d1b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fcc-frontend-libraries-project-two/static/js/2.7e46d1b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b308f479e6deb26bbe9d",
+    "url": "/fcc-frontend-libraries-project-two/static/js/main.661544a5.chunk.js"
+  },
+  {
+    "revision": "63d4a62f74c47971ed44",
+    "url": "/fcc-frontend-libraries-project-two/static/js/runtime-main.5c29899c.js"
+  }
+]);
